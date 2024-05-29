45f7f71a3e3551438d3a02f0414a600ec4bc2692
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-29 08:40:41 +0200

    added JFrame

--- src/main/java/blitz/rijks/RijksSearchFrame.java
+++ src/main/java/blitz/rijks/RijksSearchFrame.java
@@ -106,7 +106,7 @@ public class RijksSearchFrame extends JFrame {
                 imageLabel.setVerticalTextPosition(JLabel.BOTTOM);
                 imageLabel.addMouseListener(new java.awt.event.MouseAdapter() {
                     public void mouseClicked(java.awt.event.MouseEvent evt) {
-                        showLargeImage(artObject);
+                        new showLargeImage(artObject);
                     }
                 });
                 resultPanel.add(imageLabel);
@@ -123,24 +123,5 @@ public class RijksSearchFrame extends JFrame {
                 "Failed to fetch data: " + message,
                 "Error", JOptionPane.ERROR_MESSAGE);
     }
-
-    public void showLargeImage(ArtObject artObject) {
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        try {
-            URL url = new URL(artObject.webImage.url);
-            Image image = ImageIO.read(url);
-            Image scaledImage = image.getScaledInstance(
-                    800, -1, Image.SCALE_SMOOTH);
-            ImageIcon imageIcon = new ImageIcon(scaledImage);
-            JLabel imageLabel = new JLabel(imageIcon);
-
-            JScrollPane scrollPane = new JScrollPane(imageLabel);
-            add(scrollPane);
-            setVisible(true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
+
